e3a214cdc397e00ea03c7306c9a5e0c579b790bd
Aku Järvinen <aa3953@student.jamk.fi>
2023-10-10 22:14:33 +0300

    commit

--- api/index.js
+++ api/index.js
@@ -10,34 +10,57 @@ module.exports.handler = async (event) => {
     // Pura tiedosto extractFile-funktiolla
     const { filename, data } = extractFile(event);
     // Tallenna tiedosto Amazon S3:een
-    await s3.putObject({ Bucket: BUCKET, Key: filename, Body: data, ACL: 'public-read',}).promise();
+    await s3
+      .putObject({
+        Bucket: BUCKET,
+        Key: filename,
+        Body: data,
+        ACL: 'public-read',
+      })
+      .promise();
     // Palauta HTTP 200 -vastaus, joka sisältää linkin tallennettuun tiedostoon
+    const fileLink = `https://${BUCKET}.s3.amazonaws.com/${filename}`;
     return {
       statusCode: 200,
-      body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
+      body: JSON.stringify({ message: 'Success', fileLink }),
     };
-    // Jos tapahtuu virhe, palauta HTTP 500 -vastaus
   } catch (err) {
+    console.error(err);
     return {
       statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
       body: JSON.stringify({ message: err.stack }),
     };
   }
 };
 
-function extractFile(event) {
-  // Haetaan rajapinta (boundary) pyynnön Content-Type -otsikosta
-  const boundary = parseMultipart.getBoundary(event.headers['Content-Type']);
-  // Puretaan moniosainen pyyntö ja tallennetaan osat 'parts'-taulukkoon
-  const parts = parseMultipart.Parse(
-    Buffer.from(event.body, 'base64'),
-    boundary
-  );
-  // Otetaan taulukon ensimmäinen osa, joka sisältää tiedoston nimen ja datan
-  const [{ filename, data }] = parts;
-  // Palautetaan tiedoston nimi ja data objektina
-  return {
-    filename,
-    data,
-  };
-}
+const extractFile = (event) => {
+
+  try {
+    // Haetaan rajapinta (boundary) pyynnön Content-Type -otsikosta
+    const boundary = parseMultipart.getBoundary(event.headers['Content-Type']);
+    // Puretaan moniosainen pyyntö ja tallennetaan osat 'parts'-taulukkoon
+    const parts = parseMultipart.Parse(
+      Buffer.from(event.body, 'base64'),
+      boundary
+    );
+    // Otetaan taulukon ensimmäinen osa, joka sisältää tiedoston nimen ja datan
+    const [{ filename, data }] = parts;
+    // Palautetaan tiedoston nimi ja data objektina
+
+    return {
+      filename,
+      data,
+    };
+  } catch (err) {
+    console.error(err);
+    throw err; // Rethrow the error to be caught in your handler
+  }
+};
